2a9631514007f1c48bc707b26b07527b309d8412
tauz-hub <tauasanto101@gmail.com>
2022-04-23 17:19:16 -0300

    account route new

---
+++ src/routes/public/account.route.js
@@ -0,0 +1,27 @@
+import { recursiveArchivesImport } from '../../../utils/handlers-imports/handler-archives.js';
+
+export default {
+  method: 'get',
+  route: '/account',
+  run: async (req, res) => {
+    const cssDirectoriesRender = recursiveArchivesImport(
+      './src/pages/account/src/layout/css',
+      '.css'
+    );
+    console.log(cssDirectoriesRender);
+    const htmlDirectoriesRender = recursiveArchivesImport(
+      './src/pages/account/src/layout/html',
+      '.ejs'
+    );
+    const javascriptDirectoriesRender = recursiveArchivesImport(
+      './src/pages/account/src/layout/js',
+      '.js'
+    );
+
+    res.render('../../pages/account/account.ejs', {
+      cssDirectoriesRender,
+      htmlDirectoriesRender,
+      javascriptDirectoriesRender,
+    });
+  },
+};
