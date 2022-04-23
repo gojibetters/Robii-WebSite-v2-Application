cabcbc6ed088cb1d253da5746046c74ee1625271
tauz-hub <tauasanto101@gmail.com>
2022-04-23 17:19:30 -0300

    thank you route new

---
+++ src/routes/public/thankYou.route.js
@@ -0,0 +1,27 @@
+import { recursiveArchivesImport } from '../../../utils/handlers-imports/handler-archives.js';
+
+export default {
+  method: 'get',
+  route: '/thanks',
+  run: async (req, res) => {
+    const cssDirectoriesRender = recursiveArchivesImport(
+      './src/pages/thank-you/src/layout/css',
+      '.css'
+    );
+    console.log(cssDirectoriesRender);
+    const htmlDirectoriesRender = recursiveArchivesImport(
+      './src/pages/thank-you/src/layout/html',
+      '.ejs'
+    );
+    const javascriptDirectoriesRender = recursiveArchivesImport(
+      './src/pages/thank-you/src/layout/js',
+      '.js'
+    );
+
+    res.render('../../pages/thank-you/thank-you.ejs', {
+      cssDirectoriesRender,
+      htmlDirectoriesRender,
+      javascriptDirectoriesRender,
+    });
+  },
+};
